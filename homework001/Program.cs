8e4860ed3e9641f58e14a34208316aa4c3435c76
Zlataпш <sm-zlata@yandex.ru>
2022-10-29 16:28:45 +0300

    zadanie1

---
+++ homework001/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine ("Введите трехзначное число ");
+int num=Convert.ToInt32 (Console.ReadLine ());
+int amount = num.ToString().Length;
+
+if (amount < 3 || amount>3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+} 
+else
+{
+    Console.WriteLine(InCenter(num));
+}   
+ int message (string message) 
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
